90956498ab4ed09156378f7380d66bd4f05027b8
CezaryPuchowski <puchowski.cezary@gmail.com>
2023-10-01 18:50:18 +0200

    Formatted on

--- plugins/nodes_plugin.py
+++ plugins/nodes_plugin.py
@@ -86,7 +86,7 @@ $shortname $longname / $devicemodel / $battery $voltage / $snr / $lastseen
             return False
 
         response = await self.send_matrix_message(
-            room_id=room.room_id, message=self.generate_response(), formatted=False
+            room_id=room.room_id, message=self.generate_response(), formatted=True
         )
 
         return True
