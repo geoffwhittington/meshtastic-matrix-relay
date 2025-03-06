139b4d1e19d156bf360026afe2dff4b87c8fef49
takov751 <takov751@tutanota.com>
2025-03-06 03:25:16 +0000

    filter air_util_tx  just in case

--- plugins/health_plugin.py
+++ plugins/health_plugin.py
@@ -26,7 +26,9 @@ class Plugin(BasePlugin):
                     air_util_tx.append(info["deviceMetrics"]["airUtilTx"])
             if "snr" in info:
                 snr.append(info["snr"])
-        #filter out none type values from snr
+        print(str(snr))
+        #filter out none type values from snr and air_util_tx just in case
+        air_util_tx = [value for value in air_util_tx if value is not None]
         snr = [value for value in snr if value is not None]
 
         low_battery = len([n for n in battery_levels if n <= 10])
