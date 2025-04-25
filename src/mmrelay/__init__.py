454da22f36d813851817aa2299fff08b31f68367
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2025-04-24 20:09:06 -0500

    Use pkg_resources for version handling (Windows installer compatible)

--- src/mmrelay/__init__.py
+++ src/mmrelay/__init__.py
@@ -3,7 +3,15 @@ Meshtastic Matrix Relay - Bridge between Meshtastic mesh networks and Matrix cha
 """
 
 import os
+import pkg_resources
 
-# Get version from environment variable if available (set by GitHub Actions)
-# Otherwise, use a default version
-__version__ = os.environ.get("GITHUB_REF_NAME", "1.0.1")
+# First try to get version from environment variable (GitHub tag)
+if "GITHUB_REF_NAME" in os.environ:
+    __version__ = os.environ.get("GITHUB_REF_NAME")
+else:
+    # Fall back to setup.cfg metadata using pkg_resources (compatible with PyInstaller)
+    try:
+        __version__ = pkg_resources.get_distribution("mmrelay").version
+    except pkg_resources.DistributionNotFound:
+        # If all else fails, use hardcoded version
+        __version__ = "1.0.1"
