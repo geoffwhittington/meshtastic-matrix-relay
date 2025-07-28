b3b4095f40d553217468d4df467434d36cebed30
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2025-07-28 06:41:52 -0500

    Fix GPLv3 license issues
    - Add copyright notice to LICENSE file
    - Use correct PyPI trove classifier for GPLv3+

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
         "Topic :: Communications",
